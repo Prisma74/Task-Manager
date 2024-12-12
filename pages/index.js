e47d95a3bd8589a9ae13cd8a9541a4a8db464dab
Prisma74 <solopipe2001@gmail.com>
2024-12-11 19:08:17 -0500

    Installing next dependencies

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+// Declaracion de var
+
+var nombre = "Andres";
+console.log(nombre); // Output: "Andres";
+
+// Reasignacion de var
+
+nombre = "Camilo";
+console.log(nombre); // Output: "Camilo";
+
+// Declaracion con let 
+
+let edad = 25;
+console.log(edad); // Output: 25;
+
+// Reasignacion a let
+
+edad = 30;
+console.log(edad); // Output: 26;
+
+// Declaracion con const
+
+PI = 3.1416;
+console.log(PI); // Output: 3.1416;
+
+// Reasignacion de const
+
+PI = 99999;
+console.log(PI); // Error porque const no puede cambiar despues de asignarle un valor
